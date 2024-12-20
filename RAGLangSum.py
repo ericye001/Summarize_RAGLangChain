07efe2c761251f30d9c25567b30d6c67eebc44a4
ericye001 <ericye@gmail.com>
2024-12-19 22:11:49 -0800

    Update RAGLangSum.py

--- RAGLangSum.py
+++ RAGLangSum.py
@@ -69,3 +69,152 @@ model = Model(
 )
 
 flan_ul2_llm = WatsonxLLM(model=model)
+
+qa = RetrievalQA.from_chain_type(llm=flan_ul2_llm, 
+                                 chain_type="stuff", 
+                                 retriever=docsearch.as_retriever(), 
+                                 return_source_documents=False)
+query = "what is mobile policy?"
+qa.invoke(query)
+
+qa = RetrievalQA.from_chain_type(llm=flan_ul2_llm, 
+                                 chain_type="stuff", 
+                                 retriever=docsearch.as_retriever(), 
+                                 return_source_documents=False)
+query = "Can you summarize the document for me?"
+qa.invoke(query)
+
+model_id = 'meta-llama/llama-3-70b-instruct'
+
+parameters = {
+    GenParams.DECODING_METHOD: DecodingMethods.GREEDY,  
+    GenParams.MAX_NEW_TOKENS: 256,  # this controls the maximum number of tokens in the generated output
+    GenParams.TEMPERATURE: 0.5 # this randomness or creativity of the model's responses
+}
+
+credentials = {
+    "url": "https://us-south.ml.cloud.ibm.com"
+}
+
+project_id = "skills-network"
+
+model = Model(
+    model_id=model_id,
+    params=parameters,
+    credentials=credentials,
+    project_id=project_id
+)
+
+
+
+llama_3_llm = WatsonxLLM(model=model)
+
+qa = RetrievalQA.from_chain_type(llm=llama_3_llm, 
+                                 chain_type="stuff", 
+                                 retriever=docsearch.as_retriever(), 
+                                 return_source_documents=False)
+query = "Can you summarize the document for me?"
+qa.invoke(query)
+
+qa = RetrievalQA.from_chain_type(llm=flan_ul2_llm, 
+                                 chain_type="stuff", 
+                                 retriever=docsearch.as_retriever(), 
+                                 return_source_documents=False)
+query = "Can I eat in company vehicles?"
+qa.invoke(query)
+
+prompt_template = """Use the information from the document to answer the question at the end. If you don't know the answer, just say that you don't know, definately do not try to make up an answer.
+
+{context}
+
+Question: {question}
+"""
+
+PROMPT = PromptTemplate(
+    template=prompt_template, input_variables=["context", "question"]
+)
+
+chain_type_kwargs = {"prompt": PROMPT}
+
+qa = RetrievalQA.from_chain_type(llm=llama_3_llm, 
+                                 chain_type="stuff", 
+                                 retriever=docsearch.as_retriever(), 
+                                 chain_type_kwargs=chain_type_kwargs, 
+                                 return_source_documents=False)
+
+query = "Can I eat in company vehicles?"
+qa.invoke(query)
+
+query = "What I cannot do in it?"
+qa.invoke(query)
+
+memory = ConversationBufferMemory(memory_key = "chat_history", return_message = True)
+
+qa = ConversationalRetrievalChain.from_llm(llm=llama_3_llm, 
+                                           chain_type="stuff", 
+                                           retriever=docsearch.as_retriever(), 
+                                           memory = memory, 
+                                           get_chat_history=lambda h : h, 
+                                           return_source_documents=False)
+
+history = []
+
+query = "What is mobile policy?"
+result = qa.invoke({"question":query}, {"chat_history": history})
+print(result["answer"])
+
+history.append((query, result["answer"]))
+
+query = "List points in it?"
+result = qa({"question": query}, {"chat_history": history})
+print(result["answer"])
+
+history.append((query, result["answer"]))
+
+query = "What is the aim of it?"
+result = qa({"question": query}, {"chat_history": history})
+print(result["answer"])
+
+def qa():
+    memory = ConversationBufferMemory(memory_key = "chat_history", return_message = True)
+    qa = ConversationalRetrievalChain.from_llm(llm=llama_3_llm, 
+                                               chain_type="stuff", 
+                                               retriever=docsearch.as_retriever(), 
+                                               memory = memory, 
+                                               get_chat_history=lambda h : h, 
+                                               return_source_documents=False)
+    history = []
+    while True:
+        query = input("Question: ")
+        
+        if query.lower() in ["quit","exit","bye"]:
+            print("Answer: Goodbye!")
+            break
+            
+        result = qa({"question": query}, {"chat_history": history})
+        
+        history.append((query, result["answer"]))
+        
+        print("Answer: ", result["answer"])
+
+qa()
+
+#Tests
+filename = 'stateOfUnion.txt'
+url = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/XVnuuEg94sAE4S_xAsGxBA.txt'
+
+wget.download(url, out=filename)
+print('file downloaded')
+
+#All you must do is change the return_source_documents to True when you create the chain. And when you print, print the ['source_documents'][0]
+
+qa = RetrievalQA.from_chain_type(llm=llama_3_llm, chain_type="stuff", retriever=docsearch.as_retriever(), return_source_documents=True)
+query = "Can I smoke in company vehicles?"
+results = qa.invoke(query)
+print(results['source_documents'][0]) ## this will return you the source content
+
+#To use the Granite model in your notebook, go to the cell where the model_id is specified to LLAMA_3_70B_INSTRUCT and replace the current model_id with the following code. Expect different results and performance when using other models.
+
+model_id = ModelTypes.GRANITE_13B_CHAT_V2
+
+#After updating, run the remaining cells in the notebook to ensure the Granite model is used for subsequent operations.
